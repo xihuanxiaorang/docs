30ae37024c9fb886c2eb32b6d7b3c17a419876da
xiaorang <15019474951@163.com>
2024-02-01 18:49:57 +0800

    feat(article): 新增内网穿透工具文章

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -12,4 +12,9 @@ export const nav: DefaultTheme.NavItem[] = [
     ],
   },
   { text: "技术碎片", link: "/技术碎片/README", activeMatch: "/技术碎片/" },
+  {
+    text: "工具",
+    link: "/工具/开发者工具/内网穿透工具",
+    activeMatch: "/工具/",
+  },
 ];
