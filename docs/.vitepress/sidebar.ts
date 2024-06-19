9bb6cb9cbb18f8e4ba47d0c8e615ccaa2ecec647
小让 <15019474951@163.com>
2024-06-19 13:28:44 +0800

    feat(leetcode-sql): 新增刷题-LeetCode-176.第二高的薪水文章

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -251,6 +251,7 @@ export const sidebar: DefaultTheme.Sidebar = {
       items: [
         { text: "1.两数之和", link: "/1.两数之和" },
         { text: "175.组合两个表", link: "/175.组合两个表" },
+        { text: "176.第二高的薪水", link: "/176.第二高的薪水" },
       ],
     },
   ],
